732b8afcda75a96767e9f09561cd7256135a1f39
Jose Hernandez <josemanuelhdez2@gmail.com>
2025-09-16 22:06:27 +0100

    Initial commit (shoplister)

---
+++ shoplister2025/scripts/setup-android.sh
@@ -0,0 +1,20 @@
+cd ~/
+sudo apt update
+sudo apt install unzip
+wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
+unzip sdk-tools-linux-4333796.zip -d Android
+rm sdk-tools-linux-4333796.zip
+sudo apt install -y lib32z1 openjdk-8-jdk
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export PATH=$PATH:$JAVA_HOME/bin
+printf "\n\nexport JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64\nexport PATH=\$PATH:\$JAVA_HOME/bin" >> ~/.bashrc
+cd Android/tools/bin
+./sdkmanager "platform-tools" "platforms;android-36" "build-tools;36.0.0"
+export ANDROID_HOME=$HOME/Android
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+printf "\n\nexport ANDROID_HOME=$HOME/Android\nexport PATH=\$PATH:\$ANDROID_HOME/tools/bin\nexport PATH=\$PATH:\$ANDROID_HOME/platform-tools" >> ~/.bashrc
+android update sdk --no-ui
+sudo apt install gradle
+gradle -v
+adb start-server
