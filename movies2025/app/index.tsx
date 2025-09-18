4432d2e4ea8a8d1878be92bdc746d3a5c6382643
Jose Hernandez <josemanuelhdez2@gmail.com>
2025-09-18 08:38:07 +0100

    test movies list, extend tailwind theme

--- movies2025/app/index.tsx
+++ movies2025/app/index.tsx
@@ -1,13 +1,15 @@
 import { Text, View } from "react-native";
+import TestMovieList from "./components/TestMovieList";
 import "./globals.css";
 
 // create a react native component showing a flatlist with static data and show it in index.tsx
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+    <View className="flex-1 bg-white pt-12">
+      <Text className="text-xl font-bold text-accent text-center mb-6">
         Welcome to Nativewind!
       </Text>
+      <TestMovieList />
     </View>
   );
 }
