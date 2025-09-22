7a17152eaa87cfa132726b1f24596daf1e87f129
Jose Hernandez <josemanuelhdez2@gmail.com>
2025-09-22 13:07:14 +0100

    trending movies feature

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -23,21 +23,21 @@ const Search = () => {
 
   useEffect(() => {
     const timeoutId = setTimeout(async () => {
-
-
       if(searchQuery.trim()) {
         await loadMovies();
-        if (movies?.length > 0) {
-          await updateSearchCount(searchQuery, movies[0])
-        }
       } else {
         reset()
       }
     }, 500);
 
     return () => clearTimeout(timeoutId);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchQuery])
+
+  useEffect(() => {
+    if (movies?.length > 0) {
+      updateSearchCount(searchQuery, movies[0])
+    }
+  }, [movies])
 
   return (
     <View className='flex-1 bg-primary'>
