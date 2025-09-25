c4254bddbb55982392dfb827bb289f522bb34acd
Jose Hernandez <josemanuelhdez2@gmail.com>
2025-09-25 13:18:37 +0100

    ♻️ store session and not sessionId

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,29 +1,29 @@
 import Background from '@/components/Background';
 import { icons } from '@/constants/icons';
-import { createAnonymousSession, getCurrentSession } from '@/services/appwrite/auth';
+import { createAnonymousSession, getCurrentSession, getUsername } from '@/services/appwrite/auth';
 import { useSessionStore } from '@/stores/sessionStore';
 import React, { useEffect } from 'react';
 import { Image, Text, TouchableOpacity, View } from 'react-native';
 
 const Profile = () => {
 
-  const sessionId = useSessionStore((state) => state.sessionId);
-  const setSessionId = useSessionStore((state) => state.setSessionId);
+  const session = useSessionStore((state) => state.session);
+  const setSession = useSessionStore((state) => state.setSession);
 
 
   useEffect(() => {
     // Recover current session, if any
     (async function() {
-      const currentSession = await getCurrentSession()
+      const currentSession = await getCurrentSession();
       if(currentSession){ 
-        setSessionId(currentSession?.$id) 
+        setSession(currentSession);
       }
-    })()
-  })
+    })();
+  }, [setSession]);
 
   const handleAnonymousLogin = async () => {
-    let session = await createAnonymousSession();
-    setSessionId(session?.$id || null);
+    let newSession = await createAnonymousSession();
+    setSession(newSession || null);
   }
 
   return (
@@ -31,9 +31,10 @@ const Profile = () => {
       <View className="flex justify-center items-center flex-1 flex-col gap-5">
         <Image source={icons.person} className="size-10" tint-color="#fff" />
         <Text className="text-gray-500 text-base">Profile</Text>
-        {sessionId ? (
-          <Text className="text-white text-xs mt-2">Session ID: {sessionId}</Text>
-        ) : (
+        {session ? (<>
+          <Text className="text-white text-xs mt-2">Session ID: {session.$id}</Text>
+          <Text className="text-white text-lg mt-2">Username: {getUsername(session)}</Text>
+        </>) : (
           <TouchableOpacity
             className="mt-4 px-4 py-2 bg-accent rounded-full"
             onPress={handleAnonymousLogin}
--- stores/sessionStore.ts
+++ stores/sessionStore.ts
@@ -1,11 +1,16 @@
+import type { Models } from 'react-native-appwrite';
 import { create } from 'zustand';
 
 interface SessionState {
   sessionId: string | null;
   setSessionId: (id: string | null) => void;
+  session: Models.Session | null;
+  setSession: (session: Models.Session | null) => void;
 }
 
 export const useSessionStore = create<SessionState>((set) => ({
   sessionId: null,
-  setSessionId: (id) => set({ sessionId: id }),
+  setSessionId: (id: string | null) => set({ sessionId: id }),
+  session: null,
+  setSession: (session: Models.Session | null) => set({ session }),
 }));
